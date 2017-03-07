de40fac0eb3743609e15326b3025eab46f766c17
Nectarios <nekkon01@gmail.com>
2017-03-07 11:19:50 +0200

    updated about

--- src/app/components/about/about.spec.ts
+++ src/app/components/about/about.spec.ts
@@ -1,34 +1,21 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async } from '@angular/core/testing';
-import { menuComponent } from './menu';
+import { aboutComponent } from './about';
 
 describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
-        menuComponent
+        aboutComponent
       ],
     });
     TestBed.compileComponents();
   });
 
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(menuComponent);
+    const fixture = TestBed.createComponent(aboutComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(menuComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(menuComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('a').textContent).toContain('app works!');
   }));
 });
