cf02445a8d4679076d5b8169740bba5f06f2af2a
Nectarios <nekkon01@gmail.com>
2017-03-07 11:32:26 +0200

    added education

---
+++ src/app/components/education/education.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'education',
+    templateUrl: './education.html',
+    styleUrls: ['./education.scss']
+})
+export class eudcationComponent {
+
+    constructor(){
+
+    }
+}
