a1c280e1bdaf88c29980e6808f361105a55eebfc
nekkon <nekkon@or-co.gr>
2017-12-18 16:50:44 +0200

    FIX: contact responsive first round of changes

--- src/app/sections/contact/contact.module.ts
+++ src/app/sections/contact/contact.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 
 import { ContactComponent } from './contact';
 import { MatInputModule, MatButtonModule } from "@angular/material";
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-    imports: [ CommonModule, MatInputModule, MatButtonModule ],
+    imports: [ CommonModule, MatInputModule, MatButtonModule, FlexLayoutModule ],
     declarations: [ ContactComponent ],
     exports:      [ ContactComponent ]
 })
