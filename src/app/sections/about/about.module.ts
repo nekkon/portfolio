4d765c8bc2a04e1f6a459156c5ec1623887fb07a
nekkon <nekkon01@gmail.com>
2017-12-20 08:04:16 +0200

    FIX: about responsive

--- src/app/sections/about/about.module.ts
+++ src/app/sections/about/about.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 
 import { AboutComponent } from './about';
 import { MatButtonModule } from "@angular/material";
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-    imports: [ CommonModule, MatButtonModule ],
+    imports: [ CommonModule, MatButtonModule, FlexLayoutModule ],
     declarations: [ AboutComponent ],
     exports:      [ AboutComponent ]
 })
