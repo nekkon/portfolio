cc71c13cd74d8a6a4105ccc539a341be41e049a6
nekkon <nekkon01@gmail.com>
2017-12-26 20:15:09 +0200

    FIX: accomplishments

--- src/app/sections/accomplishments/accomplishments.module.ts
+++ src/app/sections/accomplishments/accomplishments.module.ts
@@ -1,11 +1,11 @@
 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AccomplishmentsComponent } from './accomplishments';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-    imports: [ CommonModule ],
+    imports: [ CommonModule, FlexLayoutModule ],
     declarations: [ AccomplishmentsComponent ],
     exports:      [ AccomplishmentsComponent ]
 })
