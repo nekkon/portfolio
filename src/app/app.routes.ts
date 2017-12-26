4fa5033deb1c2084f054752e8ee48a68264dc4b3
nekkon <nekkon01@gmail.com>
2017-12-26 18:34:55 +0200

    FIX: routing for universal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,5 @@ import { Route } from '@angular/router';
 
 export const Routing:Route[] = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', loadChildren: './sections/lazy.view.module#LazyViewModule' }
+    { path: 'home', loadChildren: 'app/sections/lazy.view.module#LazyViewModule' }
 ];
