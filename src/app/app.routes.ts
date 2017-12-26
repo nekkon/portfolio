0433a35dc72f7a65289a3c232099ff7460a5360b
nekkon <nekkon01@gmail.com>
2017-12-26 17:54:29 +0200

    FIX: routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,5 @@ import { Route } from '@angular/router';
 
 export const Routing:Route[] = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', loadChildren: './views/home/home.module#HomeModule' }
+    { path: 'home', loadChildren: './sections/lazy.view.module#LazyViewModule' }
 ];
