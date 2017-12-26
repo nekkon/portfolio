a6434c0531796e383ed8a45a000ea2ae2e33c698
nekkon <nekkon01@gmail.com>
2017-12-26 17:56:55 +0200

    FIX: code formatting

--- src/app/sections/index.ts
+++ src/app/sections/index.ts
@@ -1,10 +1,8 @@
 
 export { VideoModule } from './video/video.module';
-
 export { AboutModule } from './about/about.module';
 export { ExpertinModule } from './expertin/expertin.module';
 export { ContactModule } from './contact/contact.module';
-
 export { AccomplishmentsModule } from './accomplishments/accomplishments.module';
 export { ProjectsModule } from './projects/projects.module';
 export { ExperienceModule } from './experience/experience.module';
