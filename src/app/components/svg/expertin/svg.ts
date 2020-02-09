55b916020371bb0c0f17832bbd1ae280b955a893
nekkon <nekkon01@gmail.com>
2020-02-09 14:31:17 +0000

    Added expertin svg

---
+++ src/app/components/svg/expertin/svg.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "svg-expertin",
+  templateUrl: "./svg.html",
+  styleUrls: ["./svg.scss"]
+})
+export class SVGExpertinComponent {}
