b0733244aebe3b5317f62b3b287d243a6b293987
nekkon <nekkon01@gmail.com>
2020-02-09 14:34:44 +0000

    Updated expertin section

--- src/app/sections/expertin/expertin.module.ts
+++ src/app/sections/expertin/expertin.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-
 import { ExpertinComponent } from "./expertin";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { MasteryModule, TitleModule } from "../../components/index";
+import { SVGModule } from "src/app/components/svg/svg.module";
 
 @NgModule({
-  imports: [CommonModule, MasteryModule, FlexLayoutModule, TitleModule],
+  imports: [
+    CommonModule,
+    SVGModule,
+    MasteryModule,
+    FlexLayoutModule,
+    TitleModule
+  ],
   declarations: [ExpertinComponent],
   exports: [ExpertinComponent]
 })
