9692d6e8b38ac2d7b7c1e1495cb05dc0b6edc5eb
nekkon <nekkon01@gmail.com>
2020-02-09 14:30:33 +0000

    Added divider3 svg

---
+++ src/app/components/svg/divider3/svg.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "svg-divider3",
+  templateUrl: "./svg.html",
+  styleUrls: ["./svg.scss"]
+})
+export class SVGDivider3Component {}
