d8684706313986720a8d8513f1314c7c45dbe11d
nekkon <nekkon01@gmail.com>
2020-02-09 14:31:04 +0000

    Added education svg

---
+++ src/app/components/svg/education/svg.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "svg-education",
+  templateUrl: "./svg.html",
+  styleUrls: ["./svg.scss"]
+})
+export class SVGEducationComponent {}
