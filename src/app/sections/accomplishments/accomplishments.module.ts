a42b098bf5ff04c21bd1ef16f9b4dfb01c7b8bc2
nekkon <nekkon01@gmail.com>
2020-02-09 14:32:17 +0000

    Updated accomplishments

--- src/app/sections/accomplishments/accomplishments.module.ts
+++ src/app/sections/accomplishments/accomplishments.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from "@angular/common";
 import { AccomplishmentsComponent } from "./accomplishments";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { TitleModule } from "../../components/index";
+import { SVGModule } from "src/app/components/svg/svg.module";
 
 @NgModule({
-  imports: [CommonModule, FlexLayoutModule, TitleModule],
+  imports: [CommonModule, SVGModule, FlexLayoutModule, TitleModule],
   declarations: [AccomplishmentsComponent],
   exports: [AccomplishmentsComponent]
 })
