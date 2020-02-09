66b83d7afa5f54052f18978bb473304468d838a6
nekkon <nekkon01@gmail.com>
2020-02-09 14:31:28 +0000

    Added github svg

---
+++ src/app/components/svg/github/svg.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "svg-github",
+  templateUrl: "./svg.html",
+  styleUrls: ["./svg.scss"]
+})
+export class SVGGithubComponent {}
