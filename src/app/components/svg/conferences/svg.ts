0bcd232e37c8bb04be5aa28eccf395d56b847838
nekkon <nekkon01@gmail.com>
2020-02-09 14:29:36 +0000

    Added conferences svg

---
+++ src/app/components/svg/conferences/svg.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "svg-conferences",
+  templateUrl: "./svg.html",
+  styleUrls: ["./svg.scss"]
+})
+export class SVGConferencesComponent {}
