7c92c4733097c2faea41c7632a4f8811c41a475e
nekkon <nekkon01@gmail.com>
2020-02-09 14:30:47 +0000

    Added divider4 svg

---
+++ src/app/components/svg/divider4/svg.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "svg-divider4",
+  templateUrl: "./svg.html",
+  styleUrls: ["./svg.scss"]
+})
+export class SVGDivider4Component {}
