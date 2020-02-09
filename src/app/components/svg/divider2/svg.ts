c3a0bf2d2edbc609e8bde2785a57fbc64fbd8640
nekkon <nekkon01@gmail.com>
2020-02-09 14:30:14 +0000

    Added divider2 svg

---
+++ src/app/components/svg/divider2/svg.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "svg-divider2",
+  templateUrl: "./svg.html",
+  styleUrls: ["./svg.scss"]
+})
+export class SVGDivider2Component {}
