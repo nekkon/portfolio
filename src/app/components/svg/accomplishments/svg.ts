483d938fa575e7654e2db4cfa42ebafdd3526abe
nekkon <nekkon01@gmail.com>
2020-02-09 14:29:21 +0000

    Added accomplishments svg

---
+++ src/app/components/svg/accomplishments/svg.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "svg-accomplishments",
+  templateUrl: "./svg.html",
+  styleUrls: ["./svg.scss"]
+})
+export class SVGAccomplishmentsComponent {}
